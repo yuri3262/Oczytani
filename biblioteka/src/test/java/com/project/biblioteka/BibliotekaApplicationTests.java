1a17da98c7ae330883f9ea163f34e14f614275ba
grevorski <74430404+grevorski@users.noreply.github.com>
2021-03-30 01:23:04 +0200

    Spring project setup

---
+++ biblioteka/src/test/java/com/project/biblioteka/BibliotekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.biblioteka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
