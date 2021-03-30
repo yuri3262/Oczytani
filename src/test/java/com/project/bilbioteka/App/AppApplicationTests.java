8759a18035475b69f60eae0f3fc7f77b7f54f789
DPachota <dawid.pachota@gmail.com>
2021-03-30 15:27:22 +0000

    change structure

---
+++ src/test/java/com/project/bilbioteka/App/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bilbioteka.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
