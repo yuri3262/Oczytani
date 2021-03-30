682a02eb0377bfd156b476bf38fdae2b02eb138d
Michal <michal.m8833@gmail.com>
2021-03-30 19:57:32 +0200

    add User class

---
+++ src/main/java/com/project/bilbioteka/App/user/User.java
@@ -0,0 +1,44 @@
+package com.project.bilbioteka.App.user;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+
+    @Id
+    private Long id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
