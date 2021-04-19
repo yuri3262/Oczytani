2613aca0e6011a0e52593387892a7e444ddca744
Dawid Pachota <dawid.pachota@gmail.com>
2021-04-19 23:44:39 +0200

    few tests, mail external

---
+++ src/test/java/com/project/bilbioteka/App/registration/RegistrationServiceTests.java
@@ -0,0 +1,29 @@
+package com.project.bilbioteka.App.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class RegistrationServiceTests {
+
+    @Resource
+    RegistrationService registrationService;
+
+    @Test
+    public void givenRequest_whenRegister_thenGetOk() {
+        RegistrationRequest registrationRequest = new RegistrationRequest("john", "john2@doe.com", "password");
+        String token = registrationService.register(registrationRequest);
+
+        assertEquals("confirmed", registrationService.confirmToken(token));
+        assertTrue(registrationService.isConfirmed(token));
+
+    }
+
+
+}
+
--- src/test/java/com/project/bilbioteka/App/user/AppUserServiceTests.java
+++ src/test/java/com/project/bilbioteka/App/user/AppUserServiceTests.java
@@ -1,16 +1,11 @@
-package com.project.bilbioteka.App;
+package com.project.bilbioteka.App.user;
 
-import com.project.bilbioteka.App.user.AppUser;
-import com.project.bilbioteka.App.user.UserRole;
-import com.project.bilbioteka.App.user.AppUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 import javax.annotation.Resource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AppUserServiceTests {
@@ -23,6 +18,14 @@ class AppUserServiceTests {
         AppUser user = new AppUser("john", "john@doe.com", "password", UserRole.USER );
 
         String token = userService.signUpUser(user);
+        AppUser user2 = userService.findAppUserByEmail(user.getEmail());
+        assertNotEquals("password", user2.getPassword());
+        assertFalse(user2.isEnabled());
+
+        userService.enableUser(user.getEmail());
+
+        AppUser user3 = userService.findAppUserByEmail(user.getEmail());
+        assertTrue(user3.isEnabled());
     }
 
 
