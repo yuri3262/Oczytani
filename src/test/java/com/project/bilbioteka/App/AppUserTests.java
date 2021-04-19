22ff982abfc57eb818192876e087056435f64d3b
Dawid Pachota <dawid.pachota@gmail.com>
2021-04-19 21:25:24 +0200

    app prop fix

--- src/test/java/com/project/bilbioteka/App/AppUserTests.java
+++ src/test/java/com/project/bilbioteka/App/AppUserTests.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class UserTests {
+class qAppUserTests {
 
     @Resource
     private AppUserRepository userRepository;
