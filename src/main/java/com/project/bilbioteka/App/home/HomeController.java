d7cddb57ea3bbb5a86f52062905c445f1ca50013
Grevorski <bartek.mamak@gmail.com>
2021-04-23 16:52:05 +0200

    es como es

--- src/main/java/com/project/bilbioteka/App/home/HomeController.java
+++ src/main/java/com/project/bilbioteka/App/home/HomeController.java
@@ -1,8 +1,14 @@
 package com.project.bilbioteka.App.home;
 
+import com.project.bilbioteka.App.registration.RegistrationRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -11,5 +17,16 @@ public class HomeController {
     public String homepage(Model model) {
         return "home";
     }
+    // Login form
+    @RequestMapping("/login")
+    public String login(@ModelAttribute("user") RegistrationRequest registrationRequest,
+                        HttpServletRequest request,
+                        Errors errors) {
+        return "login";
+    }
 
+    @RequestMapping("/home_after_login")
+    public String afterLogin() {
+        return "home_after_login";
+    }
 }
