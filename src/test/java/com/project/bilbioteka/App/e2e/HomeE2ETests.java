573bf0793d53f994a39062983377a3c56cadcc48
Dawid Pachota <dawid.pachota@gmail.com>
2021-04-27 22:02:27 +0200

    [OC-106] Home Page E2E

--- src/test/java/com/project/bilbioteka/App/e2e/HomeE2ETests.java
+++ src/test/java/com/project/bilbioteka/App/e2e/HomeE2ETests.java
@@ -1,7 +1,9 @@
 package com.project.bilbioteka.App.e2e;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,10 +16,12 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 class HomeE2ETests {
 
+    private final String testURL = "https://oczytani.azurewebsites.net";
+
     private URL driverURL;
     {
         try {
-            driverURL = new URL("http://localhost:9515/wd/hub");
+            driverURL = new URL("http://localhost:9515"); ///wd/hub
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -26,11 +30,25 @@ class HomeE2ETests {
     private WebDriver driver = new RemoteWebDriver(driverURL, DesiredCapabilities.chrome());
 
     @Test
-    public void shouldBeAbleEnterUserNameAndClickSubmitToVerifyWelcomeMessage()
+    public void pageElements()
     {
-        driver.get("https://oczytani.azurewebsites.net/");
-
+        driver.get(testURL);
         assertEquals("Welcome!", driver.getTitle());
+
+        WebElement signUpLink = driver.findElement(new By.ByLinkText("SignUp"));
+        signUpLink.click();
+        assertEquals(testURL + "/registration", driver.getCurrentUrl());
+        driver.navigate().back();
+
+        WebElement logInLink = driver.findElement(new By.ByLinkText("LogIn"));
+        logInLink.click();
+        assertEquals(testURL + "/login", driver.getCurrentUrl());
+        driver.navigate().back();
+
+        WebElement logoHomeLink = driver.findElement(By.id("nav_logo"));
+        logoHomeLink.click();
+        assertEquals(testURL + "/" , driver.getCurrentUrl());
+        driver.navigate().back();
 
     }
 
