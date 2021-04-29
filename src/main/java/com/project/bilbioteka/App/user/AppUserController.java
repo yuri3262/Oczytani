230094263c426c2c3218da1b6516542bc673ab47
Grevorski <bartek.mamak@gmail.com>
2021-04-29 14:49:25 +0200

    [OC-98][OC-99][OC-100]

--- src/main/java/com/project/bilbioteka/App/user/AppUserController.java
+++ src/main/java/com/project/bilbioteka/App/user/AppUserController.java
@@ -1,13 +1,15 @@
 package com.project.bilbioteka.App.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class AppUserController {
+    @Autowired
+    private MessageSource messages;
 
     @Autowired
     private AppUserRepository userRepository;
@@ -39,4 +41,9 @@ public class AppUserController {
     public void deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
     }
+
+
+
+
+
 }
