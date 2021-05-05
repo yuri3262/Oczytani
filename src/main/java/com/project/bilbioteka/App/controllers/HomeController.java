00157f8437fab9d234c6390fe80dad5c719026f3
Michal <michal.m8833@gmail.com>
2021-05-05 19:24:34 +0200

    [OC-88][OC-89][OC-90]

--- src/main/java/com/project/bilbioteka/App/controllers/HomeController.java
+++ src/main/java/com/project/bilbioteka/App/controllers/HomeController.java
@@ -1,14 +1,9 @@
-package com.project.bilbioteka.App.home;
+package com.project.bilbioteka.App.controllers;
 
-import com.project.bilbioteka.App.registration.RegistrationRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
