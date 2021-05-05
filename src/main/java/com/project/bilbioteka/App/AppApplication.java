a23c16ded5a7b6d067d78fc5f19bc7e6ca2b4c20
Michal <michal.m8833@gmail.com>
2021-05-05 19:47:46 +0200

    fix inserting admin to database

--- src/main/java/com/project/bilbioteka/App/AppApplication.java
+++ src/main/java/com/project/bilbioteka/App/AppApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -19,6 +20,9 @@ public class AppApplication {
 
 	@Autowired
 	private AppUserService userService;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
@@ -34,7 +38,7 @@ public class AppApplication {
 				return;
 		}
 
-		AppUser admin = new AppUser("admin", "admin@admin.com", "admin", UserRole.ADMIN);
+		AppUser admin = new AppUser("admin", "admin@admin.com", bCryptPasswordEncoder.encode("admin"), UserRole.ADMIN);
 		admin.setEnabled(true);
 		userService.addUser(admin);
 	}
