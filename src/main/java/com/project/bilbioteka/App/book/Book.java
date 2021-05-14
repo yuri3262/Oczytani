241dffe9ebb47311a4cb5a14503b3cc7c7592bad
Jakub Malinowski <49591138+jakubmalinow@users.noreply.github.com>
2021-05-14 11:44:16 +0200

    Dodawanie ksiazki (#14)
    * [OC-132][OC-114]
    
    bardzo dobra robota

--- src/main/java/com/project/bilbioteka/App/book/Book.java
+++ src/main/java/com/project/bilbioteka/App/book/Book.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -19,24 +17,55 @@ import java.util.List;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
-    private String name;
+    private String title;
     private String author;
     private String publisher;
     @ElementCollection
-    private List<String> category = new ArrayList<>();
+    private List<BookCategory> category;
     private int numberOfPages;
-    private Date dateOfPublication;
-    private boolean isAvailable = false;
+    private String dateOfPublication;
+    private Boolean isAvailable;
 
 
-    public Book(String name, String author, String publisher, List<String> category, int numberOfPages, Date dateOfPublication) {
-        this.name = name;
+    public Book(String title, String author, String publisher, List<BookCategory> category, int numberOfPages, String dateOfPublication, boolean isAvailable) {
+        this.title = title;
         this.author = author;
         this.publisher = publisher;
         this.category = category;
         this.numberOfPages = numberOfPages;
         this.dateOfPublication = dateOfPublication;
+        this.isAvailable = isAvailable;
+    }
+
+    public Boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println(super.toString());
+        StringBuilder builder = new StringBuilder();
+        builder.append("id: ").append(this.id)
+                .append("\ntitle: ").append(this.title)
+                .append("\nauthor: ").append(this.author)
+                .append("\npublisher: ").append(this.publisher)
+                .append("\ncategories: ");
+
+
+        for(BookCategory cat : this.category)
+        {
+            builder.append("\n").append(cat.getName());
+        }
+
+        builder.append("\ndate od publication: ").append(this.dateOfPublication)
+                .append("\nis availabe: ").append(this.isAvailable);
+
+        return  builder.toString();
     }
 }
