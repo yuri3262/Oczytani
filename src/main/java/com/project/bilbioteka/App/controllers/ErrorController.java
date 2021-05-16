163d9cdb5e51bf385ebd0af56b51a00d32e9932b
Dawid Pachota <dawid.pachota@gmail.com>
2021-05-16 17:07:36 +0200

    error page

---
+++ src/main/java/com/project/bilbioteka/App/controllers/ErrorController.java
@@ -0,0 +1,15 @@
+package com.project.bilbioteka.App.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/error")
+    public String viewErrorPage() {
+
+        return "error";
+    }
+
+}
