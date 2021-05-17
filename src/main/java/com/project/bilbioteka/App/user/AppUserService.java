f7fc197711e66ce59615986256593daa6a50ad7d
grevorski <74430404+grevorski@users.noreply.github.com>
2021-05-17 12:12:55 +0200

    [OC-137][OC-138] Lux robota chodzi jak trzeba (#18)
    Co-authored-by: Jakub Malinowski <49591138+jakubmalinow@users.noreply.github.com>

--- src/main/java/com/project/bilbioteka/App/user/AppUserService.java
+++ src/main/java/com/project/bilbioteka/App/user/AppUserService.java
@@ -3,6 +3,7 @@ package com.project.bilbioteka.App.user;
 import com.project.bilbioteka.App.registration.token.ConfirmationToken;
 import com.project.bilbioteka.App.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,6 +20,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
+    @Autowired
     private final AppUserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final ConfirmationTokenService confirmationTokenService;
@@ -106,8 +108,8 @@ public class AppUserService implements UserDetailsService {
         userRepository.deleteById(Long.parseLong(id));
     }
 
-    public int enableUser(String email) {
-        return userRepository.enableUser(email);
+    public void enableUser(String email) {
+        userRepository.enableUser(email);
     }
 
     public boolean checkIfValidOldPassword(final AppUser user, final String oldPassword) {
@@ -119,10 +121,22 @@ public class AppUserService implements UserDetailsService {
         userRepository.save(user);
     }
 
+
+    public void updateResetPasswordToken(String token, String email){
+        AppUser user = userRepository.findUserByEmail(email);
+        if (userRepository.findByEmail(email).isPresent()) {
+            user.setResetPasswordToken(token);
+            userRepository.save(user);
+        } else throw new IllegalStateException("user not found in database");
+    }
+
+    public AppUser getByResetPasswordToken(String token) {
+        return userRepository.findByResetPasswordToken(token);
+    }
+
     public int deleteUserById(Long id){
         confirmationTokenService.deleteConfirmationTokenByUserId(id);
         return userRepository.deleteAppUser(id);
     }
-
 
 }
