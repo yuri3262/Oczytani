c28105be2f563cd598f51d4ec22f8b1767dbcf5d
jakubmalinow <jmalin99@gmail.com>
2021-05-18 22:55:50 +0200

    OC-133 backend usuwanie ksiazki

--- src/main/java/com/project/bilbioteka/App/book/BookRepository.java
+++ src/main/java/com/project/bilbioteka/App/book/BookRepository.java
@@ -1,7 +1,20 @@
 package com.project.bilbioteka.App.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Book getOne(Long aLong);
+
+
+//    @Transactional
+//    @Modifying(clearAutomatically = true)
+//    @Query("DELETE FROM book a WHERE a.id = ?1")
+//    int deleteBook(Long id);
+
 }
