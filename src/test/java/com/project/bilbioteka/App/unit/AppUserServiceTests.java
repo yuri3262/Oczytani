e2616999bf02442085c58f0bbc4febbe3fcdc6d1
Michal <michal.m8833@gmail.com>
2021-05-23 18:32:35 +0200

    [OC-103] Unit tests

--- src/test/java/com/project/bilbioteka/App/unit/AppUserServiceTests.java
+++ src/test/java/com/project/bilbioteka/App/unit/AppUserServiceTests.java
@@ -1,10 +1,15 @@
 package com.project.bilbioteka.App.unit;
 
+import com.project.bilbioteka.App.book.Book;
+import com.project.bilbioteka.App.registration.token.ConfirmationTokenRepository;
 import com.project.bilbioteka.App.user.AppUser;
+import com.project.bilbioteka.App.user.AppUserRepository;
 import com.project.bilbioteka.App.user.AppUserService;
 import com.project.bilbioteka.App.user.UserRole;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.annotation.Resource;
 
@@ -15,6 +20,15 @@ class AppUserServiceTests {
 
     @Resource
     private AppUserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private AppUserRepository userRepository;
+
+    @Autowired
+    private ConfirmationTokenRepository confirmationTokenRepository;
 
     @Test
     public void givenUser_signUpUser_thenGetOk() {
@@ -29,7 +43,100 @@ class AppUserServiceTests {
 
         AppUser user3 = userService.findAppUserByEmail(user.getEmail());
         assertTrue(user3.isEnabled());
+
+        confirmationTokenRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
+    @Test
+    public void addUsersAndGetAllOfThem_1() {
+        userRepository.deleteAll();
+        assertEquals(0, userService.getAllUsers().size());
+    }
+
+    @Test
+    public void addUsersAndGetAllOfThem_2() {
+        userRepository.deleteAll();
+        assertEquals(0, userService.getAllUsers().size());
+
+        userService.addUser(new AppUser());
+
+        assertEquals(1, userService.getAllUsers().size());
+    }
+
+    @Test
+    public void addUsersAndGetAllOfThem_3() {
+        userRepository.deleteAll();
+        assertEquals(0, userService.getAllUsers().size());
+
+        userService.addUser(new AppUser());
+        userService.addUser(new AppUser());
+        userService.addUser(new AppUser());
+
+        assertEquals(3, userService.getAllUsers().size());
+    }
+
+    @Test
+    public void getUser() {
+        userRepository.deleteAll();
+        assertEquals(0, userService.getAllUsers().size());
+
+        AppUser user = new AppUser("user1", "user1@mail.com", bCryptPasswordEncoder.encode("pass"), UserRole.USER);
+        userService.addUser(user);
+
+        assertEquals(1, userService.getAllUsers().size());
+
+        AppUser foundUser = userService.getUser(String.valueOf(user.getId()));
+
+        assertEquals(foundUser.getName(), user.getName());
+        assertEquals(foundUser.getEmail(), user.getEmail());
+        assertEquals(foundUser.getPassword(), user.getPassword());
+        assertEquals(foundUser.getRole(), user.getRole());
+    }
+
+    @Test
+    public void deleteUser() {
+        userRepository.deleteAll();
+        assertEquals(0, userService.getAllUsers().size());
+
+        userService.addUser(new AppUser("user1", "user1@mail.com", bCryptPasswordEncoder.encode("pass"), UserRole.USER));
+
+        assertEquals(1, userService.getAllUsers().size());
+
+        userService.deleteUser(String.valueOf(userService.findAppUserByName("user1").getId()));
+
+        assertEquals(0, userService.getAllUsers().size());
+    }
+
+    @Test
+    public void findUserByEmail() {
+        userRepository.deleteAll();
+        assertEquals(0, userService.getAllUsers().size());
+
+        AppUser user = new AppUser("user1", "user1@mail.com", bCryptPasswordEncoder.encode("pass"), UserRole.USER);
+        userService.addUser(user);
+
+        assertEquals(1, userService.getAllUsers().size());
+
+        AppUser foundUser = userService.findAppUserByEmail("user1@mail.com");
+
+        assertEquals(user.getName(), foundUser.getName());
+    }
+
+    @Test
+    public void changeUserPassword() {
+        userRepository.deleteAll();
+        assertEquals(0, userService.getAllUsers().size());
+
+        AppUser user = new AppUser("user1", "user1@mail.com", bCryptPasswordEncoder.encode("pass"), UserRole.USER);
+        userService.addUser(user);
+
+        assertEquals(1, userService.getAllUsers().size());
+
+        String oldPassword = user.getPassword();
+        userService.changeUserPassword(user,"pass2");
+
+        assertNotEquals(oldPassword, userService.findAppUserByName(user.getName()).getPassword());
+    }
 
 }
