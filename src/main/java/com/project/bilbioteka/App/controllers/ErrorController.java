74f9eec3f67fe2dd33deaeb6f0bd9b2386dc3c09
DPachota <dawid.pachota@kolomololabs.com>
2021-05-24 12:20:58 +0200

    error stack trace

--- src/main/java/com/project/bilbioteka/App/controllers/ErrorController.java
+++ src/main/java/com/project/bilbioteka/App/controllers/ErrorController.java
@@ -2,6 +2,10 @@ package com.project.bilbioteka.App.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.lang.*;
+import java.io.FileNotFoundException;
 
 @Controller
 public class ErrorController {
@@ -12,8 +16,20 @@ public class ErrorController {
         return "error";
     }
     @GetMapping("/error/stack")
-    public void viewErrorStackPage() {
-        Thread.dumpStack();
+    public String viewErrorStackPage() {
+        // File file = new File("test.log");
+        try
+        {
+            PrintStream pw = new PrintStream(new FileOutputStream("src/main/resources/templates/error_stack.html"));
+            Throwable t = new Throwable();
+            t.printStackTrace(pw);
+            
+        }catch (FileNotFoundException ex)  
+        {
+            ex.printStackTrace();
+        }
+        return "error_stack";
+        
     }
 
 }
